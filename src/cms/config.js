dc098126f7d2c12a555146fd2faf140091a05062
Preston Broderick <prestonbroderick@gmail.com>
2025-09-02 09:30:36 -0400

    fix: address cms error and about page alignment

--- src/cms/config.js
+++ src/cms/config.js
@@ -92,18 +92,19 @@ export const config = {
             { label: "Who We Are Text", name: "who_we_are_text", widget: "markdown" }
           ]
         },
-                {
+                        {
           file: "src/content/pages/about-us.mdx",
           label: "About Us Page",
           name: "about",
+          extension: "mdx",
+          format: "frontmatter",
           fields: [
             { label: "History Title", name: "history_title", widget: "string" },
             { label: "History Content", name: "history_content", widget: "markdown" },
             { label: "Who We Are Title", name: "who_we_are_title", widget: "string" },
-            { label: "Who We Are Content", name: "who_we_are_content", widget: "markdown" },
-            { label: "Body", name: "body", widget: "markdown", required: false }
+            { label: "Who We Are Content", name: "who_we_are_content", widget: "markdown" }
           ]
-        },
+        },,
         
         {
           file: "src/content/pages/impact.mdx",
