31422b701fe4675db5dcbe5e4875070f7a737a0b
Preston Broderick <pbroderick@imprivata.com>
2025-09-01 21:11:18 -0400

    feat: enhance projects page with automated connections and modals

--- src/cms/config.js
+++ src/cms/config.js
@@ -221,6 +221,7 @@ export const config = {
       fields: [
         { label: "Title", name: "title", widget: "string" },
         { label: "Goal", name: "goal", widget: "markdown" },
+        { label: "Description", name: "description", widget: "markdown" },
         {
           label: "Participants",
           name: "participants",
@@ -229,16 +230,6 @@ export const config = {
             { label: "Name", name: "name", widget: "string" },
             { label: "Contact", name: "contact", widget: "string" },
           ]
-        },
-        {
-          label: "Related Projects",
-          name: "related_projects",
-          widget: "relation",
-          collection: "projects",
-          multiple: true,
-          search_fields: ["title"],
-          value_field: "{{slug}}",
-          display_fields: ["title"]
         }
       ]
     },
